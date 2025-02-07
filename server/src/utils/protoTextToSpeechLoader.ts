804dad1fb22e57f5db8912f2f88061e699d4e7b5
yotaro <blog.ysk.everyday@gmail.com>
2025-02-07 21:37:34 +0900

    [add] TTSのprotoファイル対応
    扱う場合。今回は利用しない可能性大

---
+++ server/src/utils/protoTextToSpeechLoader.ts
@@ -0,0 +1,17 @@
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+import path from 'path';
+
+const PROTO_PATH = path.join(__dirname, '../../proto/text_to_speech.proto');
+
+// Protoファイルをロードする関数
+export const loadTextToSpeechProto = () => {
+  const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  });
+  return grpc.loadPackageDefinition(packageDefinition) as any;
+};
