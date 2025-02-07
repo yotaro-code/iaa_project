629346628e46c45dd66babeef891a6aa59b36b77
yotaro <blog.ysk.everyday@gmail.com>
2025-02-07 21:31:21 +0900

    [add,update] audioのgrppcのstream対応サービスコード作成

---
+++ client/lib/generated/audio_batch.pb.dart
@@ -0,0 +1,306 @@
+//
+//  Generated code. Do not modify.
+//  source: audio_batch.proto
+//
+// @dart = 2.12
+
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
+// ignore_for_file: constant_identifier_names, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_final_fields
+// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+/// 初期化リクエスト
+class InitializeRequest extends $pb.GeneratedMessage {
+  factory InitializeRequest({
+    $core.String? agentId,
+    $core.String? userId,
+  }) {
+    final $result = create();
+    if (agentId != null) {
+      $result.agentId = agentId;
+    }
+    if (userId != null) {
+      $result.userId = userId;
+    }
+    return $result;
+  }
+  InitializeRequest._() : super();
+  factory InitializeRequest.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory InitializeRequest.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(_omitMessageNames ? '' : 'InitializeRequest', package: const $pb.PackageName(_omitMessageNames ? '' : 'audio'), createEmptyInstance: create)
+    ..aOS(1, _omitFieldNames ? '' : 'agentId', protoName: 'agentId')
+    ..aOS(2, _omitFieldNames ? '' : 'userId', protoName: 'userId')
+    ..hasRequiredFields = false
+  ;
+
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  InitializeRequest clone() => InitializeRequest()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  InitializeRequest copyWith(void Function(InitializeRequest) updates) => super.copyWith((message) => updates(message as InitializeRequest)) as InitializeRequest;
+
+  $pb.BuilderInfo get info_ => _i;
+
+  @$core.pragma('dart2js:noInline')
+  static InitializeRequest create() => InitializeRequest._();
+  InitializeRequest createEmptyInstance() => create();
+  static $pb.PbList<InitializeRequest> createRepeated() => $pb.PbList<InitializeRequest>();
+  @$core.pragma('dart2js:noInline')
+  static InitializeRequest getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<InitializeRequest>(create);
+  static InitializeRequest? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get agentId => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set agentId($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasAgentId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearAgentId() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get userId => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set userId($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasUserId() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearUserId() => clearField(2);
+}
+
+/// 初期化レスポンス
+class InitializeResponse extends $pb.GeneratedMessage {
+  factory InitializeResponse({
+    $core.String? sessionId,
+    $core.List<$core.int>? initialAudioData,
+  }) {
+    final $result = create();
+    if (sessionId != null) {
+      $result.sessionId = sessionId;
+    }
+    if (initialAudioData != null) {
+      $result.initialAudioData = initialAudioData;
+    }
+    return $result;
+  }
+  InitializeResponse._() : super();
+  factory InitializeResponse.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory InitializeResponse.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(_omitMessageNames ? '' : 'InitializeResponse', package: const $pb.PackageName(_omitMessageNames ? '' : 'audio'), createEmptyInstance: create)
+    ..aOS(1, _omitFieldNames ? '' : 'sessionId', protoName: 'sessionId')
+    ..a<$core.List<$core.int>>(2, _omitFieldNames ? '' : 'initialAudioData', $pb.PbFieldType.OY, protoName: 'initialAudioData')
+    ..hasRequiredFields = false
+  ;
+
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  InitializeResponse clone() => InitializeResponse()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  InitializeResponse copyWith(void Function(InitializeResponse) updates) => super.copyWith((message) => updates(message as InitializeResponse)) as InitializeResponse;
+
+  $pb.BuilderInfo get info_ => _i;
+
+  @$core.pragma('dart2js:noInline')
+  static InitializeResponse create() => InitializeResponse._();
+  InitializeResponse createEmptyInstance() => create();
+  static $pb.PbList<InitializeResponse> createRepeated() => $pb.PbList<InitializeResponse>();
+  @$core.pragma('dart2js:noInline')
+  static InitializeResponse getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<InitializeResponse>(create);
+  static InitializeResponse? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get sessionId => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set sessionId($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasSessionId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearSessionId() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.List<$core.int> get initialAudioData => $_getN(1);
+  @$pb.TagNumber(2)
+  set initialAudioData($core.List<$core.int> v) { $_setBytes(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasInitialAudioData() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearInitialAudioData() => clearField(2);
+}
+
+/// クライアントから送信される音声データ
+class AudioRequest extends $pb.GeneratedMessage {
+  factory AudioRequest({
+    $core.String? sessionId,
+    $core.String? agentId,
+    $core.String? userId,
+    $core.int? currentRound,
+    $core.List<$core.int>? audioData,
+  }) {
+    final $result = create();
+    if (sessionId != null) {
+      $result.sessionId = sessionId;
+    }
+    if (agentId != null) {
+      $result.agentId = agentId;
+    }
+    if (userId != null) {
+      $result.userId = userId;
+    }
+    if (currentRound != null) {
+      $result.currentRound = currentRound;
+    }
+    if (audioData != null) {
+      $result.audioData = audioData;
+    }
+    return $result;
+  }
+  AudioRequest._() : super();
+  factory AudioRequest.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory AudioRequest.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(_omitMessageNames ? '' : 'AudioRequest', package: const $pb.PackageName(_omitMessageNames ? '' : 'audio'), createEmptyInstance: create)
+    ..aOS(1, _omitFieldNames ? '' : 'sessionId', protoName: 'sessionId')
+    ..aOS(2, _omitFieldNames ? '' : 'agentId', protoName: 'agentId')
+    ..aOS(3, _omitFieldNames ? '' : 'userId', protoName: 'userId')
+    ..a<$core.int>(4, _omitFieldNames ? '' : 'currentRound', $pb.PbFieldType.O3, protoName: 'currentRound')
+    ..a<$core.List<$core.int>>(5, _omitFieldNames ? '' : 'audioData', $pb.PbFieldType.OY, protoName: 'audioData')
+    ..hasRequiredFields = false
+  ;
+
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  AudioRequest clone() => AudioRequest()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  AudioRequest copyWith(void Function(AudioRequest) updates) => super.copyWith((message) => updates(message as AudioRequest)) as AudioRequest;
+
+  $pb.BuilderInfo get info_ => _i;
+
+  @$core.pragma('dart2js:noInline')
+  static AudioRequest create() => AudioRequest._();
+  AudioRequest createEmptyInstance() => create();
+  static $pb.PbList<AudioRequest> createRepeated() => $pb.PbList<AudioRequest>();
+  @$core.pragma('dart2js:noInline')
+  static AudioRequest getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<AudioRequest>(create);
+  static AudioRequest? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get sessionId => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set sessionId($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasSessionId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearSessionId() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get agentId => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set agentId($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasAgentId() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearAgentId() => clearField(2);
+
+  @$pb.TagNumber(3)
+  $core.String get userId => $_getSZ(2);
+  @$pb.TagNumber(3)
+  set userId($core.String v) { $_setString(2, v); }
+  @$pb.TagNumber(3)
+  $core.bool hasUserId() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearUserId() => clearField(3);
+
+  @$pb.TagNumber(4)
+  $core.int get currentRound => $_getIZ(3);
+  @$pb.TagNumber(4)
+  set currentRound($core.int v) { $_setSignedInt32(3, v); }
+  @$pb.TagNumber(4)
+  $core.bool hasCurrentRound() => $_has(3);
+  @$pb.TagNumber(4)
+  void clearCurrentRound() => clearField(4);
+
+  @$pb.TagNumber(5)
+  $core.List<$core.int> get audioData => $_getN(4);
+  @$pb.TagNumber(5)
+  set audioData($core.List<$core.int> v) { $_setBytes(4, v); }
+  @$pb.TagNumber(5)
+  $core.bool hasAudioData() => $_has(4);
+  @$pb.TagNumber(5)
+  void clearAudioData() => clearField(5);
+}
+
+/// サーバーから返される音声データ
+class AudioResponse extends $pb.GeneratedMessage {
+  factory AudioResponse({
+    $core.List<$core.int>? audioData,
+  }) {
+    final $result = create();
+    if (audioData != null) {
+      $result.audioData = audioData;
+    }
+    return $result;
+  }
+  AudioResponse._() : super();
+  factory AudioResponse.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory AudioResponse.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(_omitMessageNames ? '' : 'AudioResponse', package: const $pb.PackageName(_omitMessageNames ? '' : 'audio'), createEmptyInstance: create)
+    ..a<$core.List<$core.int>>(1, _omitFieldNames ? '' : 'audioData', $pb.PbFieldType.OY, protoName: 'audioData')
+    ..hasRequiredFields = false
+  ;
+
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  AudioResponse clone() => AudioResponse()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  AudioResponse copyWith(void Function(AudioResponse) updates) => super.copyWith((message) => updates(message as AudioResponse)) as AudioResponse;
+
+  $pb.BuilderInfo get info_ => _i;
+
+  @$core.pragma('dart2js:noInline')
+  static AudioResponse create() => AudioResponse._();
+  AudioResponse createEmptyInstance() => create();
+  static $pb.PbList<AudioResponse> createRepeated() => $pb.PbList<AudioResponse>();
+  @$core.pragma('dart2js:noInline')
+  static AudioResponse getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<AudioResponse>(create);
+  static AudioResponse? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.List<$core.int> get audioData => $_getN(0);
+  @$pb.TagNumber(1)
+  set audioData($core.List<$core.int> v) { $_setBytes(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasAudioData() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearAudioData() => clearField(1);
+}
+
+
+const _omitFieldNames = $core.bool.fromEnvironment('protobuf.omit_field_names');
+const _omitMessageNames = $core.bool.fromEnvironment('protobuf.omit_message_names');
